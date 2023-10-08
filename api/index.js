133c5d60b0ce5520db221dcbe6a23d4157cc4314
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-10-08 16:46:08 -0300

    Resolvido problema swagger

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json');
+const swaggerDocument = require('./swagger-output.json');
 const routes = require('./src/routes');
 
 const app = express();

0df217175d1fe6a9e973b2f23912716e80ad8c6e
Luelen Cavalheiro <luelencavalheiro@hotmail.com>
2023-10-08 16:09:56 -0300

    Adicionado rota getPrecipitation + movido funções para arquivo utils

--- src/mateomatics.service.js
+++ src/mateomatics.service.js
@@ -1,49 +1,26 @@
-const axios = require('axios');
+const { formatDate, subDays } = require('./utils');
+const { mateomaticsApi, mountQuery, formatData, DEFAULT_RETURN_FORMAT } = require('./mateomatics.utils')
 
-const { convertToBase64, formatDate, subDays, removeCSVPartFromHTML } = require('./utils');
+const getPrecipitation = async (lat, long, returnFormat = DEFAULT_RETURN_FORMAT, daysBehind = 2, precipInterval = 5) => {
+    const today = new Date();
+    const endDateFormatted = formatDate(today);
+    const startDate = subDays(today, daysBehind);
+    const startDateFormatted = formatDate(startDate);
 
-const mateomaticsApi = axios.create({ baseURL: process.env.MATEOMATICS_API_URL_BASE });
+    const datePart = `${startDateFormatted}--${endDateFormatted}`;
+    const precipPart = `:PT${precipInterval}M/precip_${precipInterval}min:mm/`;
+    const coordinatesPart = `${lat},${long}/`;
 
-const FORMAT = {
-    HTML: 'html',
-    HTML_MAP: 'html_map',
-    PNG: 'png',
-    CSV: 'csv',
-    XML: 'xml',
-    JSON: 'json',
+    const combinedParts = datePart + precipPart + coordinatesPart + returnFormat;
+
+    const query = await mountQuery(combinedParts);
+    const { data } = await mateomaticsApi.get(query);
+
+    const result = formatData(data, returnFormat, 'Precipitation');
+
+    return result;
 }
 
-const DEFAULT_RETURN_FORMAT = FORMAT.HTML;
-
-/**
- * Get token from Mateomatics API
- * 
- * @returns string
- */
-const getToken = async () => {
-    const usernamePassword = process.env.MATEOMATICS_API_USER + ':' + process.env.MATEOMATICS_API_PASSWORD;
-    const usernamePasswordBase64 = convertToBase64(usernamePassword);
-
-    const { data } = await axios.get(process.env.MATEOMATICS_LOGIN_URL, {
-        headers: {
-            'Authorization': 'Basic ' + usernamePasswordBase64
-        }
-    })
-
-    return data.access_token;
+module.exports = {
+    getPrecipitation
 }
-
-
-const mountQuery = async (query) => {
-    const accessToken = await getToken();
-
-    return query + '?access_token=' + accessToken;
-}
-
-const formatData = (data, returnFormat, title) => {
-    return returnFormat === FORMAT.HTML
-        ? removeCSVPartFromHTML(data, title)
-        : data;
-}
-
-module.exports = {}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,18 @@
 const express = require('express');
 
+const { getPrecipitation } = require('./mateomatics.service');
+
 const routes = express();
+
+routes.get('/precipitation/lat/:lat/long/:long', async (req, res) => {
+    // #swagger.description = Gives a two-day forecast oaf the expected precipitation accumulation with five minute resolution. More information: https://www.meteomatics.com/en/api/available-parameters/weather-parameter/precipitation/
+
+    const { lat, long } = req.params;
+    const { format } = req.query;
+
+    const result = await getPrecipitation(lat, long, format);
+
+    return res.status(200).send(result)
+})
 
 module.exports = routes;
